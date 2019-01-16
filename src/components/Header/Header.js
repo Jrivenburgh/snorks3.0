24f7dfb0bb52c9b56b179531dd901f791e6fea8c
loeljee <loeljee@gmail.com>
2019-01-15 20:22:49 -0800

    basic header/footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,9 +5,10 @@ import GameMessage from "../GameMessage";
 const Header = props => (
     <div className="header">
         <ul>
-            <li><h2 className=" title animated swing">Clicky Game</h2></li>
-            <GameMessage score={props.score} topScore={props.topScore} />
-            <li id="score">Score: {props.score} | Top Score: {props.topScore}</li>
+            <li><h2 className=" title animated swing">DondeDoggo</h2></li>
+            {/* <GameMessage score={props.score} topScore={props.topScore} /> */}
+            {/* <li id="score">Score: {props.score} | Top Score: {props.topScore}</li> */}
+            <li className="log"><a href="https://github.com/Jrivenburgh/snorks3.0" target="blank"><span><img className='login'/></span> Log In</a> | <a href="https://github.com/Jrivenburgh/snorks3.0" target="blank"><span><img className='signup'/></span> Sign Up</a></li>
         </ul>
         
     </div>
