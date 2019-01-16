1f8a55fbae622d6e07d80df20df125c5f22feb56
loeljee <loeljee@gmail.com>
2019-01-15 21:13:13 -0800

    slides

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,13 +1,55 @@
-import React from "react";
-import { Jumbotron } from 'reactstrap';
-import "./Instructions.css";
+import React, { Component } from "react";
+import { Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask } from "mdbreact";
 
-const Instructions = (props) => (
-    <div className="jumbo">
-        <Jumbotron style={{'background-image': `url("assets/images/mario-background.gif")`}} >
-            <h3 className=" animated fadeInDown instructions">{props.message}</h3>
-        </Jumbotron>
-    </div>
-);
+class Instructions extends Component {
+  render() {
+    return (
+      <Carousel activeItem={1} length={4} showControls={true} showIndicators={true} className="z-depth-1">
+        <CarouselInner>
+          <CarouselItem itemId="1">
+            <View>
+              <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg" alt="First slide" />
+              <Mask overlay="black-light" />
+            </View>
+            <CarouselCaption>
+              <h3 className="h3-responsive">Light mask</h3>
+              <p>First text</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem itemId="2">
+            <View>
+              <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg" alt="Second slide" />
+              <Mask overlay="black-strong" />
+            </View>
+            <CarouselCaption>
+              <h3 className="h3-responsive">Strong mask</h3>
+              <p>Second text</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem itemId="3">
+            <View>
+              <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg" alt="Third slide" />
+              <Mask overlay="black-slight" />
+            </View>
+            <CarouselCaption>
+              <h3 className="h3-responsive">Slight mask</h3>
+              <p>Third text</p>
+            </CarouselCaption>
+          </CarouselItem>
+          <CarouselItem itemId="4">
+            <View>
+              <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg" alt="Mattonit's item" />
+              <Mask overlay="black-light" />
+            </View>
+            <CarouselCaption>
+              <h3 className="h3-responsive">Sopot Beach</h3>
+              <p>Taken june 21th by @mattonit</p>
+            </CarouselCaption>
+          </CarouselItem>
+        </CarouselInner>
+      </Carousel>
+    );
+  }
+}
 
 export default Instructions;
